0b0da050aa325a21a0f12a807b613edb2d07369a
illusion-slave <84918800+illusion-slave@users.noreply.github.com>
2022-11-06 18:58:41 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -34,85 +34,85 @@ def get_info():
     return responses
 
 def get_info1():
-    url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
-    headers = {
-        "Host": "hslhapp.hhws168.com",
-        "Token":"3a90beddb879790a606bcb1141e325d9",
-        "Content-Encoding": "gzip",
-        "Versioncode":"1.2.8",
-        "Channel":"alphago",
-        "Content-Type":"application/json;charset=utf-8",
-        "Content-Length":"156",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding":"gzip",
-        "User-Agent":"okhttp/3.8.0"
-    }
-    date = "2022-11-04"
-   #date = str(datetime.date.today())
-    body = {"option0": "alphaGo",
-        "option1": "server",
-        "option2": "timeDivingGold",
-        "option3": "stockData",
-        "option4": str(date),
-        "option5": "9999",
-        "truncation": "cut20191111"
-    }
-    responses = requests.post(url = url,data = json.dumps(body),headers = headers)
-    return responses
+ url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
+ headers = {
+ "Host": "hslhapp.hhws168.com",
+ "Token":"3a90beddb879790a606bcb1141e325d9",
+ "Content-Encoding": "gzip",
+ "Versioncode":"1.2.8",
+ "Channel":"alphago",
+ "Content-Type":"application/json;charset=utf-8",
+ "Content-Length":"156",
+ "Connection": "Keep-Alive",
+ "Accept-Encoding":"gzip",
+ "User-Agent":"okhttp/3.8.0"
+ }
+   # date = "2022-10-17"
+ date = str(datetime.date.today())
+   body = {"option0": "alphaGo",
+ "option1": "server",
+ "option2": "timeDivingGold",
+ "option3": "stockData",
+ "option4": str(date),
+ "option5": "9999",
+ "truncation": "cut20191111"
+ }
+ responses = requests.post(url = url,data = json.dumps(body),headers = headers)
+ return responses
 
 def get_info2():
-    url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
-    headers = {
-        "Host": "hslhapp.hhws168.com",
-        "Token":"3a90beddb879790a606bcb1141e325d9",
-        "Content-Encoding": "gzip",
-        "Versioncode":"1.2.8",
-        "Channel":"alphago",
-        "Content-Type":"application/json;charset=utf-8",
-        "Content-Length":"156",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding":"gzip",
-        "User-Agent":"okhttp/3.8.0"
-    }
-    date = "2022-11-04"
-   # date = str(datetime.date.today())
-    body = {"option0": "alphaGo",
-        "option1": "server",
-        "option2": "minuteUpShadow",
-        "option3": "stockData",
-        "option4": str(date),
-        "option5": "9999",
-        "truncation": "cut20191111"
-    }
-    responses = requests.post(url = url,data = json.dumps(body),headers = headers)
-    return responses
+ url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
+ headers = {
+ "Host": "hslhapp.hhws168.com",
+ "Token":"3a90beddb879790a606bcb1141e325d9",
+ "Content-Encoding": "gzip",
+ "Versioncode":"1.2.8",
+ "Channel":"alphago",
+ "Content-Type":"application/json;charset=utf-8",
+ "Content-Length":"156",
+ "Connection": "Keep-Alive",
+ "Accept-Encoding":"gzip",
+ "User-Agent":"okhttp/3.8.0"
+ }
+   # date = "2022-10-17"
+ date = str(datetime.date.today())
+   body = {"option0": "alphaGo",
+ "option1": "server",
+ "option2": "minuteUpShadow",
+ "option3": "stockData",
+ "option4": str(date),
+ "option5": "9999",
+ "truncation": "cut20191111"
+ }
+ responses = requests.post(url = url,data = json.dumps(body),headers = headers)
+ return responses
 
 def get_info3():
-    url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
-    headers = {
-        "Host": "hslhapp.hhws168.com",
-        "Token":"3a90beddb879790a606bcb1141e325d9",
-        "Content-Encoding": "gzip",
-        "Versioncode":"1.2.8",
-        "Channel":"alphago",
-        "Content-Type":"application/json;charset=utf-8",
-        "Content-Length":"158",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding":"gzip",
-        "User-Agent":"okhttp/3.8.0"
-    }
-    date = "2022-11-04"
+ url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
+ headers = {
+ "Host": "hslhapp.hhws168.com",
+ "Token":"3a90beddb879790a606bcb1141e325d9",
+ "Content-Encoding": "gzip",
+ "Versioncode":"1.2.8",
+ "Channel":"alphago",
+ "Content-Type":"application/json;charset=utf-8",
+ "Content-Length":"158",
+ "Connection": "Keep-Alive",
+ "Accept-Encoding":"gzip",
+ "User-Agent":"okhttp/3.8.0"
+ }
+   date = "2022-11-04"
  # date = str(datetime.date.today())
-    body = {"option0": "alphaGo",
-        "option1": "server",
-        "option2": "minutePulseQulet",
-        "option3": "stockData",
-        "option4": str(date),
-        "option5": "9999",
-        "truncation": "cut20191111"
-    }
-    responses = requests.post(url = url,data = json.dumps(body),headers = headers)
-    return responses
+   body = {"option0": "alphaGo",
+ "option1": "server",
+ "option2": "minutePulseQulet",
+ "option3": "stockData",
+ "option4": str(date),
+ "option5": "9999",
+ "truncation": "cut20191111"
+ }
+ responses = requests.post(url = url,data = json.dumps(body),headers = headers)
+ return responses
 
 
 def parse_info(res):
@@ -156,9 +156,9 @@ def read_list(filepath):
     b.close()
     return out
 
-def push_info(filepath,list_result,template_id):
-    appid = "wx112f057de987d19e"
-    screct = "0c9af8144a9f98b2ece14e8a0918778d"
+def push_info(filepath,list_result,template_id,web_hook):
+    appid = "wxf418fb3a4ff3873f"
+    screct = "dc73193225971a9d2c747794c1f50cd9"
     #template_id = "Vos7ef7UiEXRXL6AfLsA83zbOD9Ehkd9nQ79OMLKuYg"
 
     last_list = read_list(filepath)
@@ -175,7 +175,8 @@ def push_info(filepath,list_result,template_id):
     if len(list_result) != len(last_list):
         # 数据数量不一致，直接推送
         for i in range(len(list_result)):
-            WechatMessagePush(appid, screct, template_id).send_wechat_temple_msg(list_result[i])
+            #WechatMessagePush(appid, screct, template_id).send_wechat_temple_msg(list_result[i])
+            push_report(push_list[i],web_hook)
         print("数据数量不一致，直接推送")
 
         for i in range(len(list_result)):
@@ -194,11 +195,38 @@ def push_info(filepath,list_result,template_id):
 
         if len(push_list):
             for i in range(len(push_list)):
-                WechatMessagePush(appid, screct, template_id).send_wechat_temple_msg(push_list[i])
+                #WechatMessagePush(appid, screct, template_id).send_wechat_temple_msg(push_list[i])
+                push_report(push_list[i],web_hook)
         else:
             print("没有数据更新,不推送")
 
     save_list(filepath,list_result)
+
+    def push_report(push_dict,web_hook):
+        header = {
+               "Content-Type": "application/json;charset=UTF-8"
+           }
+           message_body = {
+               "msgtype": "markdown",
+               "markdown": {
+                   "content":
+                           "##### •  code:%s \n" % + push_dict['code']} +
+                           "##### •  name：%s \n" % + push_dict['name'] +
+                           "##### •  getTime：%s \n" % + push_dict['getTime'] +
+                           "##### •  firstGetTime：%s \n" +% push_dict['firstGetTime']
+               },
+               "at": {
+                   "atMobiles": [],
+                   "isAtAll": False
+               }
+           }
+           send_data = json.dumps(message_body)  # 将字典类型数据转化为json格式
+           ChatBot = requests.post(url=web_hook, data=send_data, headers=header)
+           opener = ChatBot.json()
+           if opener["errmsg"] == "ok":
+               print(u"%s 通知消息发送成功！" % opener)
+           else:
+               print(u"通知消息发送失败，原因：{}".format(opener))
 
 if __name__ == '__main__':
     filepath = "./data_source_list.txt"
@@ -213,11 +241,11 @@ if __name__ == '__main__':
     list_result1 = parse_info(res1)
     list_result2 = parse_info(res2)
     list_result3 = parse_info(res3)
-    template_id="LwjaQf2BqPDKtoatVisuxQLKcorVC3zeLYcwQespDsY"
-    #template_id1="6EMjOCJZKHTF7fZA6QylsNcIlDiObfKw67Ddvj2Zm7Q"
-    #template_id2="Vos7ef7UiEXRXL6AfLsA83zbOD9Ehkd9nQ79OMLKuYg"
-    #template_id3="ikOqvUyQjFbDD7kqW1as22YScnOAvm8aRirGl-GW2wc"
-    push_info(filepath,list_result,template_id)
-    #push_info(filepath1,list_result1,template_id1)
-    #push_info(filepath2,list_result2,template_id2)
-    #push_info(filepath3,list_result3,template_id3)
+    webhook = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=f38433df-3a2a-46d3-bd45-8d31bf8adc94"
+    webhook1 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8eed9b02-38de-4d20-939c-278214128005"
+    webhook2 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9b6a9289-c683-4f42-9a77-04c4384cf19e"
+    webhook3 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0810ff88-06be-46dc-9d67-07e1d894ad95"
+    push_info(filepath,list_result,template_id,webhook)
+    push_info(filepath1,list_result1,template_id1,webhook1)
+    push_info(filepath2,list_result2,template_id2,webhook2)
+    push_info(filepath3,list_result3,template_id3,webhook3)
