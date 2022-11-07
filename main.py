3df69a40b74e0c0da7ed87077889606911606823
illusion-slave <84918800+illusion-slave@users.noreply.github.com>
2022-11-07 11:41:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import datetime
 import json
 import requests
-
+ 
 from WeChatPush import WechatMessagePush
-
+ 
 def get_info():
     url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
     headers = {
@@ -18,10 +18,10 @@ def get_info():
         "Accept-Encoding":"gzip",
         "User-Agent":"okhttp/3.8.0"
     }
-
+ 
     # date = "2022-11-04"
     date = str(datetime.date.today())
-
+ 
     body = {"option0": "alphaGo",
             "option1": "server",
             "option2": "minuteLargeDdePulseQulet",
@@ -32,7 +32,7 @@ def get_info():
     }
     responses = requests.post(url = url,data = json.dumps(body),headers = headers)
     return responses
-
+ 
 def get_info1():
     url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
     headers = {
@@ -59,7 +59,7 @@ def get_info1():
     }
     responses = requests.post(url = url,data = json.dumps(body),headers = headers)
     return responses
-
+ 
 def get_info2():
     url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
     headers = {
@@ -86,7 +86,7 @@ def get_info2():
     }
     responses = requests.post(url = url,data = json.dumps(body),headers = headers)
     return responses
-
+ 
 def get_info3():
     url = "http://hslhapp.hhws168.com/common/v1/jsonDownload/"
     headers = {
@@ -113,8 +113,8 @@ def get_info3():
     }
     responses = requests.post(url = url,data = json.dumps(body),headers = headers)
     return responses
-
-
+ 
+ 
 def parse_info(res):
     json1 = json.loads(res.text)
     dict1 = dict(json1)
@@ -132,21 +132,21 @@ def parse_info(res):
         # print(dict_result)
         if dict_result['getTime'] != dict_result['firstGetTime']:
             list3.append(dict_result.copy())
-
+ 
     return list3
-
+ 
     # for i in range(len(list3)):
     #     print(list3[i])
-
+ 
 def save_list(filepath,c_list):
-
+ 
     c_list = json.dumps(c_list)
     '''将c_list存入文件
     '''
     a = open(filepath, "w", encoding='UTF-8')
     a.write(c_list)
     a.close()
-
+ 
 def read_list(filepath):
     '''读取data_source_list文件
     '''
@@ -155,30 +155,34 @@ def read_list(filepath):
     out = json.loads(out)
     b.close()
     return out
-
+ 
 def push_info(filepath,list_result,web_hook):
     appid = "wx112f057de987d19e"
     screct = "0c9af8144a9f98b2ece14e8a0918778d"
     #template_id = "Vos7ef7UiEXRXL6AfLsA83zbOD9Ehkd9nQ79OMLKuYg"
-
+ 
     last_list = read_list(filepath)
     push_list = list()
-
+ 
     print("list_result")
     for i in range(len(list_result)):
        print(list_result[i])
-
+    
+    if len(list_result)
+    else:
+       print("no result")
+ 
     print("last_list")
     for i in range(len(last_list)):
         print(last_list[i])
-
+ 
     if len(list_result) != len(last_list):
         # 数据数量不一致，直接推送
         for i in range(len(list_result)):
             #WechatMessagePush(appid, screct, template_id).send_wechat_temple_msg(list_result[i])
             push_report(list_result[i],web_hook)
         print("数据数量不一致，直接推送")
-
+ 
         for i in range(len(list_result)):
            print(list_result[i])
     else:
@@ -186,23 +190,23 @@ def push_info(filepath,list_result,web_hook):
         for i in range(len(list_result)):
             if list_result[i] != last_list[i]:
                 push_list.append(list_result[i].copy())
-
+ 
         print("数据数量一致，判断更新推送")
-
+ 
         print("push_list")
         for i in range(len(push_list)):
            print(push_list[i])
-
+ 
         if len(push_list):
             for i in range(len(push_list)):
                 #WechatMessagePush(appid, screct, template_id).send_wechat_temple_msg(push_list[i])
                 push_report(push_list[i],web_hook)
         else:
             print("没有数据更新,不推送")
-
+ 
     save_list(filepath,list_result)
-
-
+ 
+ 
 def push_report(push_dict,web_hook):
     header = {
         "Content-Type": "application/json;charset=UTF-8"
@@ -211,9 +215,11 @@ def push_report(push_dict,web_hook):
         "msgtype": "text",
         "text": {
             "content":
-                " •  code："+ push_dict['code'] +
-                " •  name："+ push_dict['name'] +
-                " •  getTime：" + push_dict['getTime'] +
+                " •  code："+ push_dict['code'] +\n
+                " •  name："+ push_dict['name'] +\n
+                "https://m.10jqka.com.cn/stockpage/hs_"+push_dict['code'] +\n
+                " •  getTime：" + push_dict['getTime'] +\n
+                
                 " •  firstGetTime："+ push_dict['firstGetTime']
         },
         "at": {
@@ -228,7 +234,7 @@ def push_report(push_dict,web_hook):
         print(u"%s 通知消息发送成功！" % opener)
     else:
         print(u"通知消息发送失败，原因：{}".format(opener))
-
+ 
 if __name__ == '__main__':
     filepath = "./data_source_list.txt"
     filepath1 = "./data_source_list_1.txt"
